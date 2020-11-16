e8e149abd78fd087eb628d7f0400e86821b4bbc4
Jake Itegboje <jakeitegsy@yahoo.com>
2020-11-16 16:04:34 -0600

    Update pwd_gen.py

--- pwd_gen.py
+++ pwd_gen.py
@@ -1,63 +1,100 @@
 #! usr/bin/env python
 """ 
 Password Generator
-    Creates an Alpha numeric password using this combination
-        - present continuous verb e.g. Reading
+    Creates an Alpha numeric password from a permutation of
+        - non-noun e.g. Reading
         - noun e.g Giraffe
+        - color e.g. Blue
         - random 2 digit number
     i.e.
-        <verb>ing<noun><digitn-1><digitn>
-        ReadingGiraffe42
+        <non-noun><color><noun><digitn-1><digitn>
+        ReadingBlueGiraffe42
 """
 import string
+
 from secrets import choice
 from itertools import permutations
 
-def random_digit():
-    digits = string.digits
-    return choice(digits)
+class Generator:
 
-def random_color():
-    colors =("Red", "Orange", "Yellow", "Green", "Cyan", 
-             "Blue", "Indigo", "Violet")
-    return choice(colors)
-    
-def random_noun():
-    with open("nouns.txt") as f:
-        noun_list = f.readlines()
-        
-    return choice(noun_list).strip(" \n").title()
-    
-def random_verb():
-    """Return a verb in the present continuous i.e. -ing form"""
-    with open("verbs.txt") as f:
-        verb_list = f.readlines()
-    return choice(verb_list).strip(" \n").title()
-    
-def random_order():
-    color = random_color()
-    noun = random_noun()
-    verb = random_verb()
-    digits = f"{random_digit()}{random_digit()}"
-    password = (f"{noun}", f"{color}", f"{verb}")
-    number = choice((2, 3))
-    possibilities = [i for i in permutations(password, number)]
-    
-    if number == 2:
-        first, second = choice(possibilities)
-        final = [i for i in permutations((first, second, digits), 3)]
-        first, second, third = choice(final)
-        return f"{first}{second}{third}"
-    else:
-        first, second, third = choice(possibilities)
-        final = [i for i in permutations((first, second, third, digits), 4)]
-        first, second, third, fourth = choice(final)
-        return f"{first}{second}{third}{fourth}"
+    def __init__(self):
+        self.nouns = self.get_words("nouns")
+        self.non_nouns = self.get_words("non_nouns")
+        self.colors = self.get_words("colors")
 
-def generate_password():
-    """Return a password of the form ColorNounVerbDigitDigit in random order except for digits at the end"""
-    print(f"{random_order()}")
+    def get_words(self, filename):
+        with open(f"{filename}.txt") as file:
+            return list(set(file.readlines()))
+
+    def get_random_word(self, list):
+        return "".join(choice(list).title().strip().split())
+
+    @staticmethod
+    def get_random_digits(number=4):
+        return "".join(choice(string.digits) for i in range(number))
+
+    def generate_password(self):
+        color = get_random_word("colors")
+        noun = get_random_word("nouns")
+        non_noun = get_random_word("non_nouns")
+        digits = get_random_digits()
+        password = (noun, color, non_noun, digits)
+        return "".join(choice([i for i in permutations(password)]))
+    
+    def generate_passwords(self, number):
+        return [generate_password() for i in range(number)]
     
 if __name__ == "__main__":
-    print("Here's a password suggestion for you...")
-    generate_password()
+    print("Here are 20 password suggestions for you...")
+    generator = Generator()
+    generator.generate_passwords(20)
+#! usr/bin/env python
+""" 
+Password Generator
+    Creates an Alpha numeric password from a permutation of
+        - non-noun e.g. Reading
+        - noun e.g Giraffe
+        - color e.g. Blue
+        - random 2 digit number
+    i.e.
+        <non-noun><color><noun><digitn-1><digitn>
+        ReadingBlueGiraffe42
+"""
+import string
+
+from secrets import choice
+from itertools import permutations
+
+class Generator:
+
+    def __init__(self):
+        self.nouns = self.get_words("nouns")
+        self.non_nouns = self.get_words("non_nouns")
+        self.colors = self.get_words("colors")
+
+    def get_words(self, filename):
+        with open(f"{filename}.txt") as file:
+            return list(set(file.readlines()))
+
+    def get_random_word(self, list):
+        return "".join(choice(list).title().strip().split())
+
+    @staticmethod
+    def get_random_digits(number=4):
+        return "".join(choice(string.digits) for i in range(number))
+
+    def generate_password(self):
+        color = get_random_word("colors")
+        noun = get_random_word("nouns")
+        non_noun = get_random_word("non_nouns")
+        digits = get_random_digits()
+        password = (noun, color, non_noun, digits)
+        return "".join(choice([i for i in permutations(password)]))
+    
+    def generate_passwords(self, number):
+        return [generate_password() for i in range(number)]
+    
+if __name__ == "__main__":
+    print("Here are 20 password suggestions for you...")
+    generator = Generator()
+    generator.generate_passwords(20)
