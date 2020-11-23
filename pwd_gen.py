95d66ccfa386d0eae7a678b891d0803d34663485
Jake Itegboje <jakeitegsy@yahoo.com>
2020-11-23 09:50:27 -0600

    Update pwd_gen.py

--- pwd_gen.py
+++ pwd_gen.py
@@ -34,26 +34,35 @@ class Generator:
 
     def __init__(self):
         self.nouns = get_words("nouns")
-        self.non_nouns = get_words("non_nouns")
+        self.others = get_words("others")
         self.colors = get_words("colors")
 
     def generate_password(self):
-        color = get_random_word(self.colors)
+        #color = get_random_word(self.colors)
+        color = get_random_word(
+            ("Red", "Orange", "Green", "Yellow", "Blue", "Indigo", "Violet")
+        )
         noun = get_random_word(self.nouns)
-        non_noun = get_random_word(self.non_nouns)
+        other = get_random_word(self.others)
         digits = get_random_digits(2)
-        password = (noun, color, non_noun, digits)
+        password = (
+            noun,
+            color,
+            other,
+            digits
+        )
         return joiner(choice([i for i in permutations(password)]))
     
     def generate_passwords(self, number=1):
         return [self.generate_password() for i in range(number)]
     
 if __name__ == "__main__":
+    number_of_passwords = 10
     try:
         number_of_passwords = int(sys.argv[1])
-    except (KeyError, ValueError) as error:
-        print("Defaulting to 10 passwords because of the following error: ", error, "\n")
-        number_of_passwords = 10
-    print(f"Here are {number_of_passwords} password suggestions for you...")
-    generator = Generator()
-    print(generator.generate_passwords(number_of_passwords))
+    except (KeyError, ValueError, IndexError) as error:
+        print("Defaulting to 10 passwords:\n")
+    finally:
+        print(f"Here are {number_of_passwords} password suggestions for you...")
+        generator = Generator()
+        print(generator.generate_passwords(number_of_passwords))
